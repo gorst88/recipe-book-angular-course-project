4d3c6d8703a0dbcd8af855af842ca81a7d21b94a
Goran <stoykov.g@outlook.de>
2022-12-26 18:38:04 +0100

    feat: Http

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipesComponent } from './recipes/recipes.component';
 import { SelectARecipeComponent } from './select-a-recipe/select-a-recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -14,8 +15,16 @@ const appRoutes: Routes = [
     children: [
       { path: '', component: SelectARecipeComponent },
       { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent },
-      { path: ':id/edit', component: RecipeEditComponent },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipesResolverService],
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipesResolverService],
+      },
     ],
   },
   { path: 'shopping-list', component: ShoppingListComponent },
